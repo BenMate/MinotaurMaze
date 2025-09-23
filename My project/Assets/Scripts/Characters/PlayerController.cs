0cf739be182a5d27eb3cd97ecdb4a8e1035986c8
HowsTheGoin <78331659+BenMate@users.noreply.github.com>
2025-09-23 16:09:51 +1000

    Added: zoom Updated: playerPrefab
    Added Zoom
    Updated: player prefab now has a box collider (forgot to save it in prefab)
    Updated: player input script to gain mouse inputs

--- My project/Assets/Scripts/Characters/PlayerController.cs
+++ My project/Assets/Scripts/Characters/PlayerController.cs
@@ -6,15 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     [Header("- Move Settings -")]
     [SerializeField] private float moveSpeed = 5f;
+    [Header("- Cam Settings -")]
+    [SerializeField] private float zoomSpeed = 5.0f;
+    [SerializeField] private float minZoom = 1.0f;
+    [SerializeField] private float maxZoom = 10.0f;
     [Header("- Components -")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private PlayerInput inputs;
+    [SerializeField] private Camera cam;
+
 
     void Awake()
     {
         // Auto-assign components if missing
         rb ??= GetComponent<Rigidbody2D>();
         inputs ??= GetComponent<PlayerInput>();
+        cam ??= GetComponentInChildren<Camera>();
 
         if (rb == null)
             Debug.LogError("PlayerController requires a Rigidbody2D!");
@@ -24,6 +31,17 @@ public class PlayerController : MonoBehaviour
         // Freeze rotation and disable gravity for top-down movement
         rb.gravityScale = 0f;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void Update()
+    {
+        //camera zoom
+        float scroll = inputs.mouseWheel;
+        if (scroll != 0.0f)
+        {
+            cam.orthographicSize -= scroll * zoomSpeed;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+        }
     }
 
     void FixedUpdate()
--- My project/Assets/Scripts/Characters/PlayerInput.cs
+++ My project/Assets/Scripts/Characters/PlayerInput.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public Vector2 moveInputs;
+    
+    //mouse values
+    [HideInInspector] public bool rightClicked;
+    [HideInInspector] public bool isRickClicking;
+    [HideInInspector] public bool leftClicked;
+    [HideInInspector] public bool isLeftClicking;
+    [HideInInspector] public float mouseWheel;
 
     void Update()
     {
@@ -13,6 +20,14 @@ public class PlayerInput : MonoBehaviour
 
     void UpdateInputs()
     {
+        //Mouse
+        moveInputs.x = Input.GetAxis("Horizontal");
+        moveInputs.y = Input.GetAxis("Vertical");
+        mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+        rightClicked = Input.GetMouseButtonDown(1);
+        leftClicked = Input.GetMouseButtonDown(0);
+        isLeftClicking = Input.GetMouseButton(0);
+
         //Mouse
         moveInputs.x = Input.GetAxis("Horizontal");
         moveInputs.y = Input.GetAxis("Vertical");
